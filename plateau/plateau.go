bf3cb673c4735a7d4940c64d8a56c4125ead3c65
Lubomir Haralampiev <lubomir.haralampiev@deutschebahn.com>
2019-03-27 17:58:26 +0100

    add the rover to the plateau after processing the instructions

--- plateau/plateau.go
+++ plateau/plateau.go
@@ -1,7 +1,12 @@
 package plateau
 
+import (
+	"mars-rover-golang/rover"
+)
+
 // Plateau defines how a plateau looks like
 type Plateau struct {
-	SizeX int
-	SizeY int
+	SizeX  int
+	SizeY  int
+	Rovers []rover.Rover
 }
